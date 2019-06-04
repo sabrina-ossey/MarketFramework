6e66c1754eddb46ba4f65da0ddbb2051afdb950d
Sabrina Ossey <amansabrina.ossey@gmail.com>
2019-06-04 16:33:38 +0200

    commit micro-services 4

---
+++ agreementManagerService/src/app.js
@@ -0,0 +1,51 @@
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	cors = require('cors');
+
+  var app = express();
+
+	// CORS(CROSS_ORIGIN_RESSOURCE_SHARING) HANDLING
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); //url access control
+  res.header("Access-Control-Allow-Headers",
+   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+ );
+ // Check if the http request is equal to the options
+ if (req.method ==='OPTIONS'){
+   res.header("Access-Control-Allow-Method", "PUT, POST, PATCH, DELETE, GET" );
+   return res.status(200).json({}); //return response status with a json data load
+ }
+next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+var mongoUri = 'mongodb://localhost:27017/agreement';
+
+mongoose.connect(mongoUri, {
+	useCreateIndex: true,
+	useNewUrlParser: true
+}, function(err, res) {
+
+	if (err) {
+		return console.error('Error connecting to "%s":', mongoUri, err);
+	}
+	console.log('Connected successfully to "%s"', mongoUri);
+});
+
+const templateRoutes = require('./routes/templates');
+const agreementsRoutes = require('./routes/agreements');
+
+// Routes handle REST API request
+app.use('/templates', templateRoutes);
+app.use('/agreements', agreementsRoutes);
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000);
---
+++ agreementManagerService/src/routes/agreements.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Agreement = require('../models/agreement');
+
+router.post('/register', (req, res) => {
+
+  const agreement = new Agreement({
+    _id: new mongoose.Types.ObjectId(),
+    term: req.body.term,
+    ttype: req.body.ttype,
+    description: req.body.description,
+    purpose: req.body.purpose,
+    owner: req.body.owner,
+    consumer: req.body.consumer,
+    dob: req.body.dob,
+    compensation: req.body.compensation,
+    laws: req.body.laws,
+    nonsensitive: req.body.nonsensitive,
+    maximumavailable: req.body.maximumavailable,
+    downtime: req.body.downtime,
+    monthly: req.body.monthly,
+    transactionpm: req.body.transactionpm,
+    staticaccess: req.body.staticaccess,
+    dataprocessor: req.body.dataprocessor,
+    access: req.body.access,
+    licence: req.body.licence
+  });
+  agreement
+  .save()
+  .then(result =>{
+    console.log(result);
+    res.status(201).json({
+      message: 'Agreement created'
+    });
+  })
+  .catch( err =>{
+    console.log(err);
+    res.status(500).json({
+      error:err
+    });
+  });
+
+});
+
+router.get('/:agreementId',(req, res, next) => {
+  const id = req.params.agreementId;
+  Agreement.findById(id).select('description')
+     .exec()
+     .then(agreement =>{
+       console.log("From database", agreement);
+       if (agreement) {
+         res.status(200).json({
+             token: agreement
+           });
+       } else{
+         res.status(404).json({message: 'No valid entry found for provided ID'}); // if document not fin in the database
+       }
+     })
+     .catch(err => {
+       console.log(err);
+       res.status(500).json({error:err});//For invalid document entry
+     });
+});
+
+
+
+module.exports = router;
