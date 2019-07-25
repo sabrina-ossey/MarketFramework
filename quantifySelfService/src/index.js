3a3a44b54b9b99072c00712e61b038e3bc5bc7f5
Sabrina Ossey <amansabrina.ossey@gmail.com>
2019-07-25 11:00:02 +0200

    commit micro-services 6

--- quantifySelfService/src/index.js
+++ quantifySelfService/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+var mongoose = require('mongoose')
+    , _ = require( 'lodash' )
+	, quantifyData = require( './quantifyData.json' );
 const amqp = require('amqplib');
 const fs = require('fs');
 var path = require('path');
@@ -152,6 +154,61 @@ app.get ('/', (req, res) => {
  listenForResults();
 
 // start the https server
+
+//// INITILIZEDS with mockdata
+
+
+
+exports.reset = function( req, res ) {
+
+	// get refs to the models we defined above
+	var Food = mongoose.model( 'Food' );
+	var LogEntry = mongoose.model( 'LogEntry' );
+
+	// clear all existing documents from the collections
+	Food.find().remove();
+	LogEntry.find().remove();
+
+	// populate the foods collection from json data
+	// nothing fancy here as Food documents do not reference anything else
+	for( var i = 0; i < foodData.length; i++ ) {
+		new Food( foodData[ i ] ).save();
+	}
+
+	// now that the collection is populated we iterate over it
+	Food.find( function( err, foods ) {
+		var foodMap = {};
+
+		// store _ids of Food documents that Mongo generated upon insert
+		for( var i = 0; i < foods.length; i++ ) {
+			var food = foods[i];
+			// I am mapping the ids to the food names because the LogEntry
+			// JSON data contained this field thanks to the original source
+			// data's structure (a spreadsheet).
+			// You could utilize a more sophisticated lookup here if necessary.
+			foodMap[ food.name ] = food._id;
+		}
+
+		// populate the LogEntries collection from json data
+		for( i = 0; i < logData.length; i++ ) {
+			var logEntry = logData[ i ];
+			// we find and store food._id on LogEntry for reference
+			logEntry._food = foodMap[ logEntry.food_name ];
+
+			// note that only the fields defined in the schema will be
+			// persisted to Mongo, so the foodName field we used for
+			// lookup will not be unnecessarily added to the db
+			new LogEntry( logEntry ).save();
+		}
+	} );
+
+	res.redirect( "/" );
+};
+
+
+
+
+//////////////////////////////////
 
 https.createServer({
   key: fs.readFileSync(path.resolve(__dirname, 'keys/client.key')),
